1e7a502dd19638d5dc4a316ef1ccef9c519306c3
Hugo Barsacq <hugo.barsacq.40@gmail.com>
2023-01-15 21:04:33 +0100

    feat: header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from "next";
-import Head from "next/head"; 
+import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Apple Redesign</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets build Apple Redesign</h1>
+      <Header />
     </div>
   );
 };
