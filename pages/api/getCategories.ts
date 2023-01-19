12edbbdc61f3eefe19b5be781226d01df5587473
Hugo Barsacq <hugo.barsacq.40@gmail.com>
2023-01-19 16:49:07 +0100

    update: getCategories

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+const query = groq`*[_type == "category"] {
+  _id,
+    ...
+  }`;
+
 type Data = {
   categories: Category[];
 };
