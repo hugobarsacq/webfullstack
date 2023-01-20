e536e3e5989b2fbd3a0ce227219b9ff9806af7b0
Hugo Barsacq <hugo.barsacq.40@gmail.com>
2023-01-20 11:30:38 +0100

    feat: fetchProducts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,24 @@ import type { GetServerSideProps } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Landing from "../components/Landing";
+import Product from "../components/Product";
 import { fetchCategories } from "../utils/fetchCategories";
+import { fetchProducts } from "../utils/fetchProducts";
 
 interface Props {
   categories: Category[];
+  products: Product[];
 }
 
-const Home = ({ categories }: Props) => {
-  console.log(categories);
+const Home = ({ categories, products }: Props) => {
+  console.log(products);
+
+  const showProducts = (category: number) => {
+    return products
+      .filter((product) => product.category._ref === categories[category]._id)
+      .map((product) => <Product product={product} key={product._id} />); // filter products by category
+  };
+
   return (
     <div className="">
       <Head>
@@ -48,10 +58,10 @@ const Home = ({ categories }: Props) => {
               ))}
             </Tab.List>
             <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
-              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
               <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
               <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
-              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
             </Tab.Panels>
           </Tab.Group>
         </div>
@@ -65,10 +75,12 @@ export default Home;
 // Backend Code
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const categories = await fetchCategories();
+  const products = await fetchProducts();
 
   return {
     props: {
       categories,
+      products,
     },
   };
 };
